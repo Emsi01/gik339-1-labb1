630ed03a9fe9cd394d3c5e3e3c3b002a5083182d
Emsi01 <h22emmru@du.se>
2023-11-24 16:58:33 +0100

    Fixat uppgift 5 och gjort en anonym funktion till checkbox

--- script.js
+++ script.js
@@ -1,25 +1,32 @@
 // Hämtar variabler 
 const checkBox = document.getElementById("divstyle");
 const textField = document.getElementsByClassName("textfield"); 
-const myButton =  document.getElementsByTagName("myButton");
+const myButton =  document.getElementsByTagName("button");
 const myDiv = document.querySelector(".myDiv");
 
-
-// fördefinerad funktion 
-function klickadeBox (e) {
-    console.log('checkBox clicked, eventobjekt', e)
-}
-
-checkBox.addEventListener('click', klickadeBox); {
-}; 
-
-
-function handleBlur(e) {
-    console.log('handleBlur, eventobjekt', e);
+// fördefinerad funktion för inputfälten
+function klickadeInput(e) {
+    console.log('Inputfält klickad, eventobjekt', e);
     const name = e.target.name;
     const value = e.target.value;
 
-    const html = '<p> fältet ' + name + ' har värdet ' + value + '</p>';
+    const html = '<p> Fältet ' + name + ' har värdet ' + value + '</p>';
 
-    myDiv.insertAdjacentHTML('afterbegin',html ); 
+    myDiv.insertAdjacentHTML('afterbegin', html);
 }
+
+// Iterera över textfält och eventlyssnare för textfälten 
+for (let i = 0; i < textField.length; i++) {
+    textField[i].addEventListener('input', klickadeInput);
+}
+
+
+
+// Anonym funktion 
+const klickadeBox = function(e) {
+    console.log('checkBox clicked, eventobjekt', e)
+}
+// eventlyssnare för checkBox 
+checkBox.addEventListener('click', klickadeBox); {
+}; 
+
